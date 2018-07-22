c6eb20ec75dcaf83c6a81401fd2941e6c9f121a4
mstwrt <mstwrt@gmail.com>
2018-07-22 16:34:44 -0600

    First commit

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func buttonpushed(_ sender: UIButton)
+    {
+        print("Button pushed")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
